3cf8aea24ecc16b971dad4c4dd460ca71a945e7c
Nicholas M. Lombardo <nml125@ice.cs.rutgers.edu>
2022-11-17 22:33:02 -0500

    cd works, white space removal from front of input

--- shell.c
+++ shell.c
@@ -9,61 +9,60 @@
 char** split_buf(char* buf);
 void print_array(int arr_len, char** arr);
 void rm_ws(char* s, char a);
-
+char* rm_wsb(char* s, char a);
 
 //to do
 // 1. split buf into multiple strings to put into argv 
 // (which is supposed to be a char**)
 // 2. remove spaces from strings
+// 3. ensure "ls " doesn't raise error
 int main(){
     char* buf = (char*)malloc(100);
     char* cwd = (char*)malloc(100);
     char* pth_buf = (char*)malloc(100);
-    
+
     int off = 0;
     //shell loop
     while(1){
-        printf("%s> ", getcwd(cwd, 100));
+        printf("> ");
         off = scanf("%[^\n]%*c", buf);
         
         char **argv = (char**)malloc(100);
-        argv = split_buf(buf);
+     ;   argv = split_buf(buf);
 
         // this is cd
         if(strcmp(argv[0], "cd") == 0){
             chdir(argv[1]);
-            printf("%s\n", getcwd(pth_buf, 100));
-            goto bottom;
         } 
 
-        pid_t pid = fork();
-        // child process
-        if (pid == 0){              
-              
-            //first check bin
-            snprintf(pth_buf, 100, "/bin/%s", argv[0]);
-            execv(pth_buf, argv);
-            //then check this directory
-            if (errno == 2){
-                snprintf(pth_buf, 100, "./%s", argv[0]);
+        else{
+            pid_t pid = fork();
+            // child process
+            if (pid == 0){              
+                  
+                //first check bin
+                snprintf(pth_buf, 100, "/bin/%s", argv[0]);
                 execv(pth_buf, argv);
-            } 
-            //finally check given path
-            if (errno == 2){
-                execv(argv[0], argv);
+                //then check this directory
+                if (errno == 2){
+                    snprintf(pth_buf, 100, "./%s", argv[0]);
+                    execv(pth_buf, argv);
+                } 
+                //finally check given path
+                if (errno == 2){
+                    execv(argv[0], argv);
+                }
+               //if nothing works, then print error 
+                if (errno == 2){
+                    perror("command not found");
+                }
+                exit(127);
             }
-           //if nothing works, then print error 
-            if (errno == 2){
-                perror("program does not exists");
+            else{   //parent process
+
+                waitpid(pid, 0, 0);
             }
-            exit(127);
         }
-        else{   //parent process
-
-            waitpid(pid, 0, 0);
-        }
-
-bottom:
         if (off != 1)
             break;
     }
@@ -74,9 +73,7 @@ bottom:
     return 0;
 }
 
-
-
-
+//prints array
 void print_array(int arr_len, char** arr){
     for (int i = 0; i < arr_len ;i ++)
         printf("%s, ", arr[i]);
@@ -87,9 +84,17 @@ void print_array(int arr_len, char** arr){
 char** split_buf(char* buf){
     
     int spaces = 0; 
-    char* buf_cpy = malloc(100);
+
+    char* buf_cpy0 = malloc(100);
+    char* buf_cpy;
+
     char** argv = malloc(100);
-    strcpy(buf_cpy, buf);
+    
+   //remove white space from front of string -- i.e., "   ls" -> "ls"
+    strcpy(buf_cpy0, buf);
+
+    buf_cpy = rm_wsb(buf_cpy0, ' ');
+   
     argv[0] = buf_cpy;
     for (int i = 0; i < 100; i ++){
 
@@ -107,17 +112,14 @@ char** split_buf(char* buf){
     // this is to ensure that I can free bud_cpy
 
     for(int i =0; i < spaces + 1 ; i++){
-        // first i have to allocate mem to save the string. I'll use 
-        // strlen to get the size to allocate
-        // then I copy arv[i] into that allocated mem
-        // finally I replace arv[i] with the allocated mem
         char* token_cpy = (char*)malloc(strlen(argv[i]));
         strcpy(token_cpy, argv[i]);
         rm_ws(token_cpy, ' ');
         argv[i] = token_cpy;
     }
+
     argv[spaces + 2] = NULL;
-    free(buf_cpy);
+    free(buf_cpy0);
     return argv;
 }
 
@@ -131,6 +133,15 @@ void rm_ws(char* s, char a) {
         }
     } while (*s++ = *d++);
 }
+
+//removes white space in beg of string
+char* rm_wsb(char* s, char a) {
+    char* d = s;
+    while (*d == a)
+        ++d;
+    return d;
+}
+
 
 
 
