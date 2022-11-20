d625de09d1c15fff67c250154ef17ce669629745
Nicholas M. Lombardo <nml125@ice.cs.rutgers.edu>
2022-11-20 09:03:01 -0500

    starting to work on signals

--- shell.c
+++ shell.c
@@ -6,104 +6,122 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-char** split_buf(char* buf);
-void print_array(int arr_len, char** arr);
+#define SIZE 256
+
+char** gen_argv(char* buf);
+void print_array(char** arr);
 char* rm_ws(char* str);
 char* rm_wsb(char* s, char a);
+void freeargv(char** argv);
+
 
 //to do
-// 1. split buf into multiple strings to put into argv 
-// (which is supposed to be a char**)
-// 2. remove spaces from strings
-// 3. ensure "ls " doesn't raise error
+// 1. make sure i can free() malloced blocks
+// 2. the other shit!
 int main(){
-    char* buf = (char*)malloc(100);
-    char* cwd = (char*)malloc(100);
-    char* pth_buf = (char*)malloc(100);
+    char* buf = (char*)malloc(SIZE);
+    char* pth_buf = (char*)malloc(SIZE);
 
     int off = 0;
     //shell loop
-    while(1){
+    while(1)
+    {
+        //command line prompt "> "
         printf("> ");
         off = scanf("%[^\n]%*c", buf);
         
-        char **argv = (char**)malloc(100);
-        argv = split_buf(buf);
+        // pointer to argument array
+        char **argv;
+
+        // generate argument array with raw command line input
+        argv = gen_argv(buf);
 
         // this is cd
         if(strcmp(argv[0], "cd") == 0){
             chdir(argv[1]);
         } 
-
+        
+        // run program from command line
         else{
+            //first create child process
             pid_t pid = fork();
+
             // child process
             if (pid == 0){              
                   
-                //first check bin
-                snprintf(pth_buf, 100, "/bin/%s", argv[0]);
+                //first check bin/*
+                snprintf(pth_buf, SIZE, "/bin/%s", argv[0]);
                 execv(pth_buf, argv);
-                //then check this directory
+
+                //then check current directory
                 if (errno == 2){
-                    snprintf(pth_buf, 100, "./%s", argv[0]);
+                    snprintf(pth_buf, SIZE, "./%s", argv[0]);
                     execv(pth_buf, argv);
                 } 
-                //finally check given path
+                // finally check given path
                 if (errno == 2){
                     execv(argv[0], argv);
                 }
+
                //if nothing works, then print error 
                 if (errno == 2){
                     perror("command not found");
                 }
+                free(buf);
+                free(pth_buf);
+                freeargv(argv);
+                free(argv);
                 exit(127);
-            }
-            else{   //parent process
-
+            } 
+            //parent process
+            else{
+                freeargv(argv);  
+                free(argv);
                 waitpid(pid, 0, 0);
             }
         }
         if (off != 1)
             break;
     }
+    
     free(buf);
-    free(cwd);
     free(pth_buf);
 
     return 0;
 }
 
 //prints array
-void print_array(int arr_len, char** arr){
-    for (int i = 0; i < arr_len ;i ++)
+void print_array(char** arr){
+    for (int i = 0; arr[i] != NULL ;i ++)
         printf("%s, ", arr[i]);
 }
 
 // this function creates argv from buf.
-// to do: create a copy of each string.
-char** split_buf(char* buf){
-    
+char** gen_argv(char* buf){
+    // spaces holds the number of spaces in buf
     int spaces = 0; 
-
-    char* buf_cpy0 = malloc(100);
-    char* buf_cpy;
-
-    char** argv = malloc(100);
     
-   //remove white space from front of string -- i.e., "   ls" -> "ls"
+    // copy buf to avoid issues with parent process
+    char* buf_cpy0 = malloc(SIZE);
+    char* buf_cpy;
     strcpy(buf_cpy0, buf);
-
     buf_cpy = rm_wsb(buf_cpy0, ' ');
     
-   // places buf_cpy into argv[0] 
-    argv[0] = buf_cpy;
-    for (int i = 0; i < 100; i ++){
+    // allocate argv
+    char** argv = malloc(SIZE);
 
-        // if the string is "echo 'hello world'"
-        // places null terminator at last space char
-        // e.g., "hello,         world" -> "hello,       '\0'world"
-        if (buf_cpy[i] == ' ' && buf_cpy[i + 1] != ' '){
-            
+    // places buf_cpy into argv[0] 
+    argv[0] = buf_cpy;
+
+    for (int i = 0; i < SIZE; i ++)
+    {
+
+        /*places null terminator at last space char, counts spaces 
+        and adds pointer to first letter in each term to argv*/
+        if (buf_cpy[i] == ' ' 
+                && buf_cpy[i + 1] != '\0'
+                && buf_cpy[i + 1] != ' ') 
+        {
             spaces ++;
             argv[spaces] = &buf_cpy[i + 1];
             buf_cpy[i] = '\0';
@@ -111,21 +129,26 @@ char** split_buf(char* buf){
         }
     }
 
-    // replace elements of argv with copies of those elements
-    // this is to ensure that I can free bud_cpy
-
-    for(int i =0; i < spaces + 1 ; i++){
-        char* token_cpy = (char*)malloc(strlen(argv[i]));
-        strcpy(token_cpy, argv[i]);
-        token_cpy = rm_ws(token_cpy);
+    /*replace elements of argv with copies, copies are malloced. 
+     To free() call free_argv*/
+    for(int i =0; i < spaces + 1 ; i++)
+    {
+        char* tmp = rm_ws(argv[i]);
+        char* token_cpy = (char*)malloc(strlen(tmp) + 1);
+        strcpy(token_cpy, tmp);
         argv[i] = token_cpy;
     }
 
-    argv[spaces + 2] = NULL;
+    //finally add NULL at the end of argv
+    argv[spaces + 1] = NULL;
+
+    //free buf_cpy0
     free(buf_cpy0);
+    
     return argv;
 }
 
+//removes ws from string
 char* rm_ws(char *str)
 {
     int nsc = 0;
@@ -143,17 +166,6 @@ char* rm_ws(char *str)
 }
 
 
-//removes white space in string
-//void rm_ws(char* s, char a) {
-//    char* d = s;
-//    do {
-//            r+d;
-//        }
-//    } while (*s++ = *d++);
-//}
-
-
-
 //removes white space in beg of string
 char* rm_wsb(char* s, char a) {
     char* d = s;
@@ -162,9 +174,16 @@ char* rm_wsb(char* s, char a) {
     return d;
 }
 
+// free the allcated copies in argv
+void freeargv(char** argv){
+    int i = 0;
+    while(argv[i] != NULL)
+    {   free(argv[i]);
+        i++;
+    }
+    free(argv[i]);
 
-
-
+}
 
 
 
